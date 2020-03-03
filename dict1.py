be38734ee4304d3402a81932cefb225614442dda
deepressure <diana.rusakovich@gmail.com>
2020-03-03 12:08:18 +0100

    Add files via upload

---
+++ dict1.py
@@ -0,0 +1,8 @@
+#add multiple values to a dictionary key with other rules provided
+def update_dictionary(d, key, value):
+    if key in d.keys():
+        d[key].append(value)
+    elif 2 * key in d.keys():
+        d[2 * key].append(value)
+    else:
+        d[2 * key] = [value]
