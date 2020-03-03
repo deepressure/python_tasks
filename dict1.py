916c7e813924dcb6e52e37a7d8a3c35a83ed8eca
deepressure <diana.rusakovich@gmail.com>
2020-03-03 12:15:31 +0100

    Update dict1.py

--- dict1.py
+++ dict1.py
@@ -6,3 +6,11 @@ def update_dictionary(d, key, value):
         d[2 * key].append(value)
     else:
         d[2 * key] = [value]
+     
+def update_dictionary(d, key, value):
+    if key in d:
+        d[key] += [value]
+    elif 2 * key in d:
+        d[2 * key] += [value]
+    else:
+        d[2 * key] = [value]
